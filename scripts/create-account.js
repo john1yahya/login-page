828597418f982ca0ec3cc78f60ca3eac949b6c2c
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-07-31 18:19:32 +0100

    fix the create.js

--- scripts/create-account.js
+++ scripts/create-account.js
@@ -1,3 +1,21 @@
-import { users } from "../data/accounts";
+import { users } from "../data/accounts.js";
+
+
  const username = document.querySelector('.create-username');
- username.value = 'asdasd'
+ 
+ const email = document.querySelector('.create-email');
+
+ const password = document.querySelector('.create-password');
+
+ const confirmPassword = document.querySelector('.confirm-password');
+
+
+
+ document.querySelector('.signInButton').addEventListener('click', () => {
+    let usernameElement = username.value;
+    let emailElement = email.value;
+    let passwordElement = password.value;
+    let confirmPasswordElement = confirmPassword.value;
+
+
+ });
