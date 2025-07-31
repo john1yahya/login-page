411b5c321cd7938169f321365d48cb896d2a05e6
john1yahya <yahya.hoummani.50@edu.uiz.ac.ma>
2025-07-31 17:41:08 +0100

    writing script inside script.js for the login page 2 functions refrech input and validation...

---
+++ data/accounts.js
@@ -0,0 +1,13 @@
+export const users = [{
+    username:'yahya hoummani',
+    email: 'example@gmail.com',
+    password:'12345'
+},{
+    username:'john ',
+    email: 'example1@gmail.com',
+    password:'123456'
+},{
+    username:'issa',
+    email: 'email',
+    password: 'password'
+}];
---
+++ scripts/log-in.js
@@ -0,0 +1,60 @@
+import {users} from "../data/accounts.js";
+
+// first step we added event listener
+
+   function stylingValidation(result){
+        let iconName ;
+        let validation ;
+        if (result){
+            validation = 'valid';
+            iconName = 'check'; 
+            
+        }else if(!result){
+            validation = 'notValid';
+            iconName = 'exclamation';
+        }
+        email.classList.add(`${validation}`);
+        password.classList.add(`${validation}`);
+
+        document.querySelectorAll('.icons').forEach((icon) => {
+            icon.innerHTML = `<img src="assets/icons/${iconName}.png" class="icon">`;
+        });
+        setTimeout(() => {
+            email.classList.remove(`${validation}`);
+            password.classList.remove(`${validation}`);
+            document.querySelectorAll('.icons').forEach((icon) => {
+            icon.innerHTML = '';
+            });
+        }, 1000)
+            
+        console.log('valid');
+    };
+    
+    function refrechInput(email, password){
+        email.value = '';
+        password.value = '';
+    }
+
+
+const email = document.querySelector('.login-email');
+
+const password = document.querySelector('.login-password');
+
+document.querySelector('.signInButton')
+    .addEventListener('click', (event) => {
+        event.preventDefault();
+        let emailElement = email.value;
+        let passwordElement = password.value;
+
+
+        let result = users.find((user) => {
+            return user.email === emailElement && user.password === passwordElement;
+        })
+
+        stylingValidation(result);
+        setTimeout(() => {
+            refrechInput(email, password)
+        }, 1000);
+    });
+
+ 
